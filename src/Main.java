308f42719c56624663858ff6824361948ca5e1ef
AT1RUZ <dtzurita2003@gmail.com>
2024-08-22 01:35:31 -0400

    CRUD Tipo de Alimento

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,19 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Servicios_Provincias servicio = ControladoraServicios.obtener_Servicios_Provincias();
-        LinkedList<Provincia> lista = servicio.getProvincias();
-        Provincia actividad = servicio.read_Provincia("P6");
+        Servicios_Tipo_Alimento servicio = ControladoraServicios.obtener_Servicios_Tipo_Alimento();
+        LinkedList<Tipo_Alimento> lista = servicio.getTiposAlimento();
+        Tipo_Alimento actividad = servicio.read_TipoAlimentoPorID("AL001");
+        Tipo_Alimento alimento = new Tipo_Alimento("AL006", "Comida Procesada");
+
+//        servicio.create_TipoAlimento(alimento);
+        servicio.delete_TipoAlimento(alimento.getIdAlimento());
 
         for (int i = 0; i < lista.size(); i++) {
-            System.out.println(lista.get(i).getNombreProvincia());
+            System.out.println(lista.get(i).getNombreAlimento());
         }
 
-        System.out.println(actividad.getNombreProvincia());
+        System.out.println(actividad.getNombreAlimento());
 
 
     }
--- src/services/Servicios_Tipo_Alimento.java
+++ src/services/Servicios_Tipo_Alimento.java
@@ -1,4 +1,90 @@
 package services;
 
+import dto.Tipo_Alimento;
+
+import java.sql.*;
+import java.util.LinkedList;
+
 public class Servicios_Tipo_Alimento {
+
+    public Servicios_Tipo_Alimento() {
+    }
+
+    public boolean create_TipoAlimento(Tipo_Alimento tipoAlimento) throws SQLException {
+        String sql = "INSERT INTO \"Tipos_de_Alimento\" (\"ID_Alimento\", \"Nombre_Alimento\") VALUES (?, ?)";
+        Connection conexion = ControladoraServicios.getConexion();
+
+        try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
+            stmt.setString(1, tipoAlimento.getIdAlimento());
+            stmt.setString(2, tipoAlimento.getNombreAlimento());
+
+            int rowsInserted = stmt.executeUpdate();
+            return rowsInserted > 0;
+        }
+    }
+
+    public Tipo_Alimento read_TipoAlimentoPorID(String idAlimento) throws SQLException {
+        String sql = "SELECT * FROM \"Tipos_de_Alimento\" WHERE \"ID_Alimento\" = ?";
+        Connection conexion = ControladoraServicios.getConexion();
+
+        try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
+            stmt.setString(1, idAlimento);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new Tipo_Alimento(
+                            rs.getString("ID_Alimento"),
+                            rs.getString("Nombre_Alimento")
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean update_TipoAlimento(Tipo_Alimento tipoAlimento) throws SQLException {
+        String sql = "UPDATE \"Tipos_de_Alimento\" SET \"Nombre_Alimento\" = ? WHERE \"ID_Alimento\" = ?";
+        Connection conexion = ControladoraServicios.getConexion();
+
+        try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
+            stmt.setString(1, tipoAlimento.getNombreAlimento());
+            stmt.setString(2, tipoAlimento.getIdAlimento());
+
+            int rowsUpdated = stmt.executeUpdate();
+            return rowsUpdated > 0;
+        }
+    }
+
+    public boolean delete_TipoAlimento(String idAlimento) throws SQLException {
+        String sql = "DELETE FROM \"Tipos_de_Alimento\" WHERE \"ID_Alimento\" = ?";
+        Connection conexion = ControladoraServicios.getConexion();
+
+        try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
+            stmt.setString(1, idAlimento);
+
+            int rowsDeleted = stmt.executeUpdate();
+            return rowsDeleted > 0;
+        }
+    }
+
+    public LinkedList<Tipo_Alimento> getTiposAlimento() throws SQLException {
+        LinkedList<Tipo_Alimento> tiposAlimento = new LinkedList<>();
+        String sql = "SELECT * FROM \"Tipos_de_Alimento\"";
+        Connection conexion = ControladoraServicios.getConexion();
+
+        try (Statement stmt = conexion.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Tipo_Alimento tipoAlimento = new Tipo_Alimento(
+                        rs.getString("ID_Alimento"),
+                        rs.getString("Nombre_Alimento")
+                );
+                tiposAlimento.add(tipoAlimento);
+            }
+        }
+        return tiposAlimento;
+    }
+
+
 }
