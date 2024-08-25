c2f3508a4abcea1d2f2a1503908bbd77519ef69f
AT1RUZ <dtzurita2003@gmail.com>
2024-08-25 00:59:22 -0400

    Inicio de lo que estaba haciendo el Rojo

--- src/visuals/paneles/PanelCreateActividad.java
+++ src/visuals/paneles/PanelCreateActividad.java
@@ -4,4 +4,10 @@ import javax.swing.*;
 
 public class PanelCreateActividad extends JPanel {
 
+    private JTextField textoDescripcion;
+    private JTable tablaTransporte;
+    private JTable tablaAnimal;
+    private JTable tablaVeterinario;
+    private JTable tablaProveedorAlimentos;
+    private JTable tablaProveedorServiciosComplementarios;
 }
--- src/visuals/paneles/PanelMostrarActividades.java
+++ src/visuals/paneles/PanelMostrarActividades.java
@@ -3,4 +3,6 @@ package visuals.paneles;
 import javax.swing.*;
 
 public class PanelMostrarActividades extends JPanel {
+    private JPanel panel1;
+    private JTable tableActividades;
 }
