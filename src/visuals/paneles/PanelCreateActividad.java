ce219755ad437dfe4bc52709484a3c3eaf015544
AT1RUZ <dtzurita2003@gmail.com>
2024-08-25 00:28:25 -0400

    Panel Crear ACtividad

--- src/visuals/paneles/PanelCreateActividad.java
+++ src/visuals/paneles/PanelCreateActividad.java
@@ -4,4 +4,20 @@ import javax.swing.*;
 
 public class PanelCreateActividad extends JPanel {
 
+    private JLabel descripcion;
+    private JTextField txtDescripcion;
+    private JLabel labelAnimal;
+    private JPanel panelTransporte;
+    private JPanel panelAnimal;
+    private JLabel lblVeterinario;
+    private JPanel panelVeterinario;
+    private JLabel lblProveedorAlimentos;
+    private JPanel panelProveedorAlimentos;
+    private JLabel lblProveedorServiciosComplementarios;
+    private JPanel panelProveedorServiciosComplementarios;
+    private JTable tableTransporte;
+    private JTable tableAnimal;
+    private JTable tableVeterinario;
+    private JTable tableProveedorAlimentos;
+    private JTable tableProveedorServiciosCOmplementarios;
 }
